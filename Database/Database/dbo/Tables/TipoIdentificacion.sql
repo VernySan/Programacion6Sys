eeb2b0d59af7603394be1bce11996007d3cb7556
verny <verny.mata@icumed.com>
2021-10-08 18:15:45 -0600

    Se incrementa el tamaño de la descripcion en la tabla TipoIdentificacion

--- Database/Database/dbo/Tables/TipoIdentificacion.sql
+++ Database/Database/dbo/Tables/TipoIdentificacion.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[TipoIdentificacion]
 (
    IdTipoIdentificacion INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_dentificacion PRIMARY KEY CLUSTERED(IdTipoIdentificacion),
-	Descripcion varchar(50) NOT NULL
+	Descripcion varchar(255) NOT NULL
 
 )WITH (DATA_COMPRESSION = PAGE)
 GO
